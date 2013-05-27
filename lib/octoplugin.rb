d90050c9caa6fd0664b62ec4d4b248d6dbcaf674
Jon Frisby <jfrisby@mrjoy.com>
2013-05-26 21:38:44 -0700

    Adding an example of how we might add Rake tasks to a project that uses the plugin...

--- lib/octoplugin.rb
+++ lib/octoplugin.rb
@@ -1,3 +1,13 @@
 class Octoplugin < Octodemo::Plugin
   VERSION='0.0.1'
+
+  def self.init_rake!(ctx)
+    puts "Setting up rake tasks!"
+    ctx.instance_eval do
+      desc "Some task."
+      task :some_task do
+        puts "Doing something!"
+      end
+    end
+  end
 end
